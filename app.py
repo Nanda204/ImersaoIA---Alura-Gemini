81468c02f72e22ffc88f30f4a18c2343bd66202d
Fernanda Buscaratto <fefebuk@gmail.com>
2025-05-21 17:05:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,8 @@ def main():
     else:
         st.error("Erro: A variável de ambiente 'GEMINI_API_KEY' não está definida. Certifique-se de configurar o Secret no Streamlit Cloud.")
         return
-
+    
+    
     ingredientes_str = st.text_input("✍️ Quais ingredientes você tem em casa? (separados por vírgula)", key=ingredientes_key, value=st.session_state[ingredientes_key]).lower()
     st.write("\n")
     preferencias = st.text_input("🤔 Você tem alguma preferência alimentar? (vegetariano, vegano, sem glúten, etc., separado por vírgula)", key=preferencias_key, value=st.session_state[preferencias_key]).lower()
